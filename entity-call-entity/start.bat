e6c36ed97c6a34648de155d7aa11609262b8eb13
weilizhen <weilizhen@ztgame.com>
2019-11-13 18:49:06 +0800

    修改启动脚本

--- entity-call-entity/start.bat
+++ entity-call-entity/start.bat
@@ -3,15 +3,6 @@
 call stop.bat
 call flushRedis.bat
 
-rem set ZEUS=zeus@v0.0.29
-set ZEUS=go-service
-rem if not exist %GOPATH%/pkg/mod/github.com/GA-TECH-SERVER/%ZEUS% (
-rem 	echo "get zeus start" 
-rem  	set GOPROXY=
-rem 	git config http.extraheader "PRIVATE-TOKEN: AiyKkDd3XFzxmnQjXGgt"
-rem 	go get -v -insecure github.com/giant-tech/go-service/%ZEUS%
-rem 	echo "get zeus stop"
-rem )
 
 set GOPROXY=https://goproxy.io
 set GOBIN=%~dp0bin
