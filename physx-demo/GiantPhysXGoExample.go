7b4414c654bddb88a47d442ad54974d0460ca2bc
yekoufeng <yekoufeng@ztgame.com>
2019-11-19 11:51:12 +0800

    重新调整文件目录

--- physx-demo/GiantPhysXGoExample.go
+++ physx-demo/GiantPhysXGoExample.go
@@ -1,35 +1,28 @@
 package main
 
-/*
-#cgo CXXFLAGS: -std=c++11
-#cgo CFLAGS: -I .
-#cgo LDFLAGS: -L ./bin -lGiantPhysXRelease_x64
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include "GiantPhysX/GxAPI.h"
-*/
-import "C"
+//import "C"
+import "time"
+import "physx-demo/GiantPhysXGo"
 
 func main() {
 	//var physics = GiantPhysXGo.GxCreatePhysics("TropicalStorm.gxgame", "192.168.133.79")
-	//var str1 = "TropicalStorm.gxgame"
-	//var str2 = "192.168.133.79"
+	var str1 = "TropicalStorm.gxgame"
+	var str2 = "192.168.133.79"
 
 	//cstr1, cstr2 := C.CString(str1), C.CString(str2)
-	//var physics = GiantPhysXGo.GxCreatePhysics(cstr1, cstr2)
+	var physics = GiantPhysXGo.GxCreatePhysics(str1, str2)
 
 	//defer C.free(unsafe.Pointer(cstr1)) // must call
 	//defer C.free(unsafe.Pointer(cstr2))
 
-	//var scene = physics.CreateScene("factory01_area04_01.gxscene")
+	var scene = physics.CreateScene("factory01_area04_01.gxscene")
 
-	/*for {
+	for {
 		scene.Update()
 		time.Sleep(1 * time.Second)
 	}
-	*/
-	//GiantPhysXGo.GxDestroyPhysics()
+
+	GiantPhysXGo.GxDestroyPhysics()
 
 	//C.GxCreatePhysics(cstr1, cstr2)
 }
